2dd767592f27ce4beec2c4d66756ba6c2b7c2017
Sebi <sebi@thesebi.dev>
2022-01-22 16:12:37 +0100

    Added basic Login
    Co-authored-by: NewtTheWolf <dominik@spitzli.dev>

---
+++ developers.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const ThreadManagerData = {
+    bot: "Thread Manager",
+    developers: [
+        {
+            // Sebi
+            id: "280094303429197844",
+        },
+        {
+            // N.T.W
+            id: "310115562305093632",
+        }
+    ]
+}
+
+const MidnightData = {
+    bot: "Midnight",
+    developers: [
+        {
+            // Sebi
+            id: "280094303429197844",
+        }
+    ]
+}
+
+
+const TipicoXData = {
+    bot: "TipicoX",
+    developers: [
+        {
+            // Sebi
+            id: "280094303429197844",
+        },
+        {
+            // Itachi
+            id: "718231134336581692",
+        },
+        {
+            // Stern
+            id: "472020196119281684",
+        }
+    ]
+}
+
+const InfinityLoungeData = {
+    bot: "Infinity Lounge",
+    developers: [
+        {
+            // Sebi
+            id: "280094303429197844",
+        },
+        {
+            // Zerul
+            id: "474732997006852097",
+        },
+        {
+            // MooVoo
+            id: "278255521629339650",
+        },
+        {
+            // N.T.W
+            id: "310115562305093632",
+        },
+        {
+            // Semicolon
+            id: "603609359989342208",
+        }
+    ]
+}
+
+module.exports = { ThreadManagerData, MidnightData, TipicoXData, InfinityLoungeData }
--- server.js
+++ server.js
@@ -10,27 +10,97 @@ const pico = require("picocolors")
 const fetch = require("node-fetch")
 const FormData = require("form-data")
 const log = console.log;
+const devs = require('./developers');
+const session = require('express-session')
 
 app.set("view-engine", "ejs")
 app.use(express.static(__dirname + '/public')); //static directory
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  expires: 604800000,
+}));
+
+// =============== FUNCTIONS =============== //
+
+const forceAuth = (req, res, next) => { // Only connect with login
+  if (!req.session.user) return res.redirect('/login')
+  else return next();
+}
+
+const validateForData = async function(bot)
+{
+  if(bot == process.env.ThreadManager) { return devs.ThreadManagerData }
+  else if(bot == process.env.Midnight) { return devs.MidnightData }
+  else if(bot == process.env.TipicoX) { return devs.TipicoXData }
+  else if(bot == process.env.InfinityLounge) { return devs.InfinityLoungeData }
+  else return false
+}
+
+// Cache
+const developers = new Set()
+// Clear Cache
+setInterval(() => {
+  developers.clear()
+}, 604800000) // 7 days
+
+const getDevelopers = async function(bot)
+{
+  //if(!developers.size)
+  //{
+    if(!(await validateForData(bot))) return; // if bot exists
+    const arrayOfIds = (await validateForData(bot)).developers
+    const link = "https://discord.com/api/v9/users"
+    let ids = []
+    let profiles = []
+
+    for(const [key, value] of Object.entries(arrayOfIds))
+    {
+      ids.push(value.id)
+    }
+
+    for(const id of ids) {
+      const profile = await fetch(`${link}/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bot ${process.env.SebisTownhallToken}`,
+        },
+      }).then((res) => res.json());
+      profiles.push(profile)
+      developers.add(profile)
+    }
+    // []
+    return profiles
+  //}
+  //return developers
+}
 
 // =============== GET =============== //
-app.get('/auth/callback/:bot', async (req, res) => {
-  const bot = req.params.bot;
+
+app.get('/login', async (req, res) => {
+  if (req.session.user) return res.redirect('/');
+  res.redirect(process.env.LOGIN_WITH_DISCORD_LINK)
+})
+
+app.get("/logout", (req, res) => {
+  req.session.destroy()
+  res.redirect("/")
+})
+
+app.get('/auth/callback', async (req, res) => {
+  if (req.session.user) return res.redirect('/');
   const code = req.query.code;
-  let id, secret, token;
-  if(bot == process.env.SebisTownhall) { id = process.env.SebisTownhallID; secret = process.env.SebisTownhallSecret, token = process.env.SebisTownhallToken }
-  else return res.json({ message: 'Application does not exist' });
   if(!code) return res.json({ message: 'Invalid access token' });
   
   const data = new FormData()
-  data.append("client_id", id);
-  data.append("client_secret", secret);
+  data.append("client_id", process.env.SebisTownhallID);
+  data.append("client_secret", process.env.SebisTownhallSecret);
   data.append("grant_type", "authorization_code");
   data.append("redirect_uri", process.env.LOCAL_AUTH_REDIRECT);
-  data.append("scope", "guilds.join identify"); // split every needed scope with space
+  data.append("scope", "guilds.join identify guilds"); // split every needed scope with space
   data.append("code", code);
   
   const discord_token = await fetch("https://discord.com/api/oauth2/token", {
@@ -44,17 +114,20 @@ app.get('/auth/callback/:bot', async (req, res) => {
       authorization: `${discord_token.token_type} ${discord_token.access_token}`,
     },
   }).then((res) => res.json());
+
+  response.tag = `${response.username}#${response.discriminator}`
+  response.avatarURL = response.avatar ? `https://cdn.discordapp.com/avatars/${response.id}/${response.avatar}.png` : null
+  req.session.user = response
   
   await fetch(`https://discord.com/api/guilds/${process.env.SebisTownhallGuildID}/members/${response.id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bot ${token}`,
+      Authorization: `Bot ${process.env.SebisTownhallToken}`,
     },
     body: JSON.stringify({ access_token: discord_token.access_token }),
   });
-  
-  return res.send("Successfully Logged in! You can close this window now!")
+  res.redirect('/')
 })
 
 app.get('/', async (req, res) => {
@@ -67,6 +140,7 @@ app.get('/', async (req, res) => {
     tipicoxinvite: process.env.TipicoInvite,
     infinityloungeinvite: process.env.InfinityLoungeInvite,
     invite: process.env.DISCORD_INVITE,
+    user: req.session.user
   })
 });
 
@@ -93,6 +167,18 @@ app.get('/:bot/index', async (req, res) => {
 app.get('/:bot/developers', async (req, res) => {
   const bot = req.params.bot
   const link = `${bot}/developers.ejs`
+  const data = await getDevelopers(bot)
+  if(!data) { return res.json({ message: 'Application does not exist' }); }
+  if(bot == process.env.ThreadManager) { res.render(link, { devs: data, bot: "Thread Manager", botname: process.env.ThreadManager }) }
+  else if(bot == process.env.Midnight) { res.render(link, { devs: data, bot: "Midnight", botname: process.env.Midnight  }) }
+  else if(bot == process.env.TipicoX) { res.render(link, { devs: data, bot: "TipicoX", botname: process.env.TipicoX  }) }
+  else if(bot == process.env.InfinityLounge) { res.render(link, { devs: data, bot: "Infinity Lounge", botname: process.env.InfinityLounge  }) }
+  else return res.json({ message: 'Application does not exist' });
+})
+
+app.get('/:bot/docs', async (req, res) => {
+  const bot = req.params.bot
+  const link = `${bot}/docs.ejs`
   if(bot == process.env.ThreadManager) { res.render(link, { bot: "Thread Manager", botname: process.env.ThreadManager }) }
   else if(bot == process.env.Midnight) { res.render(link, { bot: "Midnight", botname: process.env.Midnight  }) }
   else if(bot == process.env.TipicoX) { res.render(link, { bot: "TipicoX", botname: process.env.TipicoX  }) }
